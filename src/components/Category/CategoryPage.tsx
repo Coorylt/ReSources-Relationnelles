099c7e9efe93dbe9395b804b82af75cd7d1036fd
LETOMBE_CORY <coorylt@gmail.com>
2024-05-22 13:02:52 +0200

    add image and show cateogires

--- src/components/Category/CategoryPage.tsx
+++ src/components/Category/CategoryPage.tsx
@@ -1,88 +1,62 @@
-// import React, { useState, useEffect } from 'react';
-// import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
-// import { useTranslation } from 'react-i18next';
-// import axios from "axios";
-// import LoadingScreen from '../LoadingScreen/LoadingScreen';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import axios from "axios";
+import LoadingScreen from '../LoadingScreen/LoadingScreen';
+import getApiUrl from '../../Services/getApiUrl';
+import { styles } from './style';
 
-// type Category = {
-//     id: number;
-//     title: string;
-// };
+type Category = {
+    id: number;
+    title: string;
+    image:string;
+};
 
-// export default function CategorysPage() {
-//     const [categories, setCategories] = useState<Category[] | null>(null);
-//     const [categoriesError, setCategoriesError] = useState<string | null>(null);
-//     const { t } = useTranslation();
+export default function CategorysPage() {
+    const [categories, setCategories] = useState<Category[] | null>(null);
+    const [categoriesError, setCategoriesError] = useState<string | null>(null);
+    const { t } = useTranslation();
 
-//     useEffect(() => {
-//         const fetchCategories = async () => {
-//             try {
-//                 const categoriesResponse = await axios.get('http://127.0.0.1:8000/api/categories?page=1', {
-//                     headers: {
-//                         'Content-Type': 'application/ld+json',
-//                     },
-//                 });
-//                 setCategories(categoriesResponse.data['hydra:member']); 
-//             } catch (err: any) {
-//                 setCategoriesError(err.message || 'Erreur lors de la récupération des ressources');
-//             }
-//         };
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const categoriesResponse = await axios.get(getApiUrl('/categories?page=1'), {
+                    headers: {
+                        'Content-Type': 'application/ld+json',
+                    },
+                });
+                setCategories(categoriesResponse.data['hydra:member']);
+            } catch (err: any) {
+                setCategoriesError(err.message || 'Erreur lors de la récupération des ressources');
+            }
+        };
 
-//         fetchCategories();
-//     }, []); 
+        fetchCategories();
+    }, []);
 
-//     // if (!categories) return <LoadingScreen />;
-//     if (categoriesError) return <View><Text>{categoriesError}</Text></View>;
+    if (!categories) return <LoadingScreen />;
+    if (categoriesError) return <View><Text>{categoriesError}</Text></View>;
 
-//     return (
-//         <ScrollView>
-//             <View style={styles.container}>
-//                 <Text style={styles.heading}>Catégories</Text>
-//                 <View style={styles.categoryContainer}>
-//                     {categories.map((category) => (
-//                         <View key={category.id} style={[styles.categoryBox, { backgroundColor: `color${category.title}` }]}>
-//                             <Image source={{ uri: `/img/${category.title}.jpg` }} style={styles.categoryImage} />
-//                             <Text style={styles.categoryTitle}>{t("categories."+category.title)}</Text>
-//                         </View>
-//                     ))}
-//                 </View>
-//             </View>
-//         </ScrollView>
-//     );
-// }
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <Text style={styles.heading}>Catégories</Text>
+                <View style={styles.categoryContainer}>
+                    {categories ? (
+                        categories.map((category) => (
+                            <View key={category.id} style={[styles.categoryBox, { backgroundColor: `color${category.title}` }]}>
+                                <Image source={{ uri: `./public/img${category.title}.jpg` }} style={styles.categoryImage} />
+                                <Text style={styles.categoryTitle}>{t("category." + category.title)}</Text>
+                            </View>
+                        ))
+                    ) : (
+                        <LoadingScreen />
+                    )}
+                </View>
 
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         padding: 20,
-//     },
-//     heading: {
-//         fontSize: 24,
-//         fontWeight: 'bold',
-//         marginBottom: 20,
-//     },
-//     categoryContainer: {
-//         flexDirection: 'row',
-//         flexWrap: 'wrap',
-//         justifyContent: 'space-between',
-//     },
-//     categoryBox: {
-//         width: '45%',
-//         aspectRatio: 1, // To maintain aspect ratio
-//         borderRadius: 10,
-//         marginBottom: 20,
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//     },
-//     categoryImage: {
-//         width: '100%',
-//         height: '100%',
-//         borderRadius: 10,
-//     },
-//     categoryTitle: {
-//         fontSize: 16,
-//         fontWeight: 'bold',
-//         marginTop: 10,
-//         textAlign: 'center',
-//     },
-// });
+
+            </View>
+        </ScrollView>
+    );
+}
+
